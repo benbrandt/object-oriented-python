57ff9ab38bb1b796c0300fded1795760891704ee
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-04-03 23:46:00 -0700

    Make sure the user gets 3 breaks

--- break_time.py
+++ break_time.py
@@ -1,5 +1,9 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+breaks = 0
+
+while breaks < 3:
+    time.sleep(10)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    breaks += 1
