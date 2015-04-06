32c284d9515c8d00b3aea340e1f4c65e31ce22f5
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-04-05 23:34:19 -0700

    Fix path so file can run script without error

--- rename_files.py
+++ rename_files.py
@@ -3,8 +3,13 @@ import os
 def rename_files():
     #(1) get file names from a folder
     file_list = os.listdir(r"C:\Users\Ben\Downloads\prank\prank")
-    print(file_list)
+    saved_path = os.getcwd()
+    print("Current Working Directory is " + saved_path)
+    os.chdir(r"C:\Users\Ben\Downloads\prank\prank")
     
     #(2) for each file, rename filename
-
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
+    
 rename_files()
