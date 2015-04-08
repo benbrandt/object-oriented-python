a7e162ea434c721eb4d0c881a400200993c12746
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-04-07 21:15:01 -0700

    Draw a circle by turtle drawing 36 squares

--- mindstorms.py
+++ mindstorms.py
@@ -18,18 +18,20 @@ def draw_art():
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
-    brad.speed(2)
-    draw_square(brad)
+    #brad.speed(2)
+    for i in range(0,36):
+        draw_square(brad)
+        brad.right(10)
 
     # Create the turtle Angie - draws a circle
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
 
     # Create the turle Leonard - draws a triangle
-    leonard = turtle.Turtle()
-    draw_triangle(leonard)
+    #leonard = turtle.Turtle()
+    #draw_triangle(leonard)
         
     window.exitonclick()
 
