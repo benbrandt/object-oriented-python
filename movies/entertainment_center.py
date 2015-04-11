9a47ec6bd5e43d1ef5e17339a5ff4d247e6901cd
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-04-10 21:06:08 -0700

    Add inception to the list of movies

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -13,3 +13,10 @@ avatar = media.Movie("Avatar",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 print(avatar.storyline)
+
+inception = media.Movie("Inception",
+                        "Dom Cobb is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious.",
+                        "http://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
+                        "https://www.youtube.com/watch?v=66TuSJo4dZM")
+
+inception.show_trailer()
