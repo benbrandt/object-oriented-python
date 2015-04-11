415196b20f21f2ce8fe56da044be3990185bda8e
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-04-10 21:16:59 -0700

    Add more movies and the fresh_tomatoes script

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,22 +1,32 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life.",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-
-print(toy_story.storyline)
+                    "A story of a boy and his toys that come to life.",
+                    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                    "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-
-print(avatar.storyline)
+                    "A marine on an alien planet",
+                    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 inception = media.Movie("Inception",
-                        "Dom Cobb is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious.",
-                        "http://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
-                        "https://www.youtube.com/watch?v=66TuSJo4dZM")
+                    "Dom Cobb is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious.",
+                    "http://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
+                    "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
-inception.show_trailer()
+school_of_rock = media.Movie("School of Rock",
+                    "Using rock music to learn",
+                    "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                    "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+                    "A rat is a chef in paris",
+                    "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                    "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                    "Going back in time to meet authors",
+                    "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                    "https://www.youtube.com/watch?v=atLg2wQQxvU")
