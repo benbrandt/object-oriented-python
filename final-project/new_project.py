c3101e05576cd12813b3350a8c9de1778dc6561d
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-05-02 23:36:09 -0700

    Refactor project to create folders for Fuller projects

--- final-project/new_project.py
+++ final-project/new_project.py
@@ -2,5 +2,6 @@ import project_settings
 
 project = project_settings.Project()
 
-#project.project_directory()
 project.new_project(project.project_directory())
+
+project.create_folders(project.PROGRAMS)
--- final-project/project_settings.py
+++ final-project/project_settings.py
@@ -2,9 +2,9 @@ import os, Tkinter, Tkconstants, tkFileDialog
 
 class Project():
     """ This class allows users to create a folder structure for their project. """
-
-    VALID_PROGRAMS = ['After Effects', 'Audition', 'Illustrator', 'Photoshop', 'Premiere', 'Resolve']
-
+    
+    PROGRAMS = ['After Effects', 'Audition', 'Illustrator', 'Photoshop', 'Premiere', 'Resolve']
+    
     def __init__(self):
         self = self
 
@@ -18,7 +18,23 @@ class Project():
     def new_project(self, directory):
         self.directory = directory
 
-        os.mkdir(self.directory + "/new")
+        os.mkdir(self.directory + "/Final")
+        os.mkdir(self.directory + "/Media")
 
-    def select_programs(self, programs):
+    def create_folders(self, programs):
         self.programs = programs
+        
+        if 'After Effects' or 'Premiere' in self.programs:
+            os.mkdir(self.directory + "/Media/Video")
+        if 'After Effects' or 'Audition' or 'Premiere' in self.programs:
+            os.mkdir(self.directory + "/Media/Audio")
+        if 'Illustrator' or 'Photoshop':
+            os.mkdir(self.directory + "/Media/Graphics")
+        if 'After Effects' in self.programs:
+            os.mkdir(self.directory + "/After_Effects")
+        if 'Audition' in self.programs:
+            os.mkdir(self.directory + "/Audition")
+        if 'Premiere' in self.programs:
+            os.mkdir(self.directory + "/Premiere")
+        if 'Resolve' in self.programs:
+            os.mkdir(self.directory + "/Media/Resolve")
